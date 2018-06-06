62a609b051a6199f99660c6c0a6a33d9370cb94f
Varun Bawa <varunbawa62ak@gmail.com>
2018-06-07 00:12:49 +0530

    Made some minor functionality edits in js code

--- pop.js
+++ pop.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2017 Authenticity Checker Authors. All rights reserved.
+// Copyright (c) 2017 Authenticity Buffer Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+// Code Author: Varun Bawa
 /**
  * Get the current URL.
  *
